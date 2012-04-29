ba7e14956f914697ff8e279bd5bb8b51fc4c865b
Carlos Falgueras García <fark.zano@gmail.com>
2012-04-29 02:01:48 +0200

    Añadida función de autocalibrado al magnetómetro

--- HMC5843.cpp
+++ HMC5843.cpp
@@ -6,7 +6,7 @@ using namespace MGT_Registers;
 Magnetometer::Magnetometer() : 
 	xScale(1), yScale(1), zScale(1)
 {
-	//calibrate();
+	calibrate();
 
 	//Configuring device
 	//Will start from DATA X MSB and fetch all the others
@@ -21,14 +21,17 @@ Magnetometer::Magnetometer() :
 	Wire.send(0x01);	//Single conversion mode
 
 	Wire.endTransmission();
-}
-
-void Magnetometer::getData(s_sensor_data* sen_data){
+	
 	//Will start from DATA X MSB and fetch all the others
 	Wire.beginTransmission(ADDRESS);
 	Wire.send(REG_XH);
 	Wire.endTransmission();
 
+}
+
+void Magnetometer::getData(s_sensor_data* sen_data){
+	//The pointer to the registers of device always point to REG_XH
+	//at this point, so do not need move it. We read the 6 bytes directly
 	Wire.requestFrom(ADDRESS, 6);
 	while(Wire.available() < 6);	//Wait 6 bytes
 	//Read out the 3 values, 2 bytes each.
@@ -57,4 +60,52 @@ bool Magnetometer::dataReady(){
 	while(!Wire.available());
 
 	return (Wire.receive() & 0x01);	//RDY bit is set
+}
+
+void Magnetometer::calibrate(){
+	int x, y, z, mx=0, my=0, mz=0, max=0;
+
+	//Setting positive bias configuration requiered for calibration
+	Wire.beginTransmission(ADDRESS);
+	Wire.send(REG_CONFA);
+	Wire.send(0x19);	//Positive bias and 50Hz (not necesary)
+	Wire.endTransmission();
+
+	//Will start from DATA X MSB and fetch all the others
+	Wire.beginTransmission(ADDRESS);
+	Wire.send(REG_XH);
+	Wire.endTransmission();
+  
+	for(int i=10 ; i > 0; --i){ 
+		//Wait to data conversion
+		while(!dataReady());
+
+		Wire.requestFrom(ADDRESS, 6);
+		while(Wire.available() < 6);	//Wait 6 bytes
+
+		x = (Wire.receive() << 8) | Wire.receive();
+		y = (Wire.receive() << 8) | Wire.receive();
+		z = (Wire.receive() << 8) | Wire.receive();
+
+		//Keep the maximum of each axis
+		if (x>mx) mx=x;
+		if (y>my) my=y;
+		if (z>mz) mz=z;
+		
+		//Request a new conversion
+		Wire.beginTransmission(ADDRESS);
+		Wire.send(REG_MODE);
+		Wire.send(0x01);	//Single conversion mode
+		Wire.endTransmission();
+	}
+
+	//Keep the maximum of three axis
+	if(mx>max) max=mx;
+	if(my>max) max=my;
+	if(mz>max) max=mz;
+	
+	//Calc scales
+	xScale = (float) max/mx;
+	yScale = (float) max/my;
+	zScale = (float) max/mz;
 }
