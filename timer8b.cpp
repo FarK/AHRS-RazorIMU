efdb354dec4bab41d91f763412d0c05102fa594c
Carlos Falgueras García <fark.zano@gmail.com>
2012-07-07 15:17:10 +0200

    Implementadas funciones start,pause,resume,stop y reset.
    Eliminado el método getDeltaTime. Se necesita medir varios intervalos de
    tiempo, esto es mejor hacerlo en de manera externa a la clase.

--- timer8b.cpp
+++ timer8b.cpp
@@ -1,16 +1,35 @@
 #include "timer8b.h"
 #include <avr/io.h>
 
+using namespace Timer8b_Const;
+
 Timer8b::Timer8b(){
-	TCCR0A = _BV(COM0A0) | _BV(COM0A1) | ~_BV(COM0B0) | ~_BV(COM0B1) |
-		~_BV(WGM01) | ~_BV(WGM00);
+	//Timer configuration for normal mode. Initially timer is stopped.
+	TCCR0A = (uint8_t) ~_BV(COM0A0) & ~_BV(COM0A1) & ~_BV(COM0B0) & ~_BV(COM0B1) &
+		~_BV(WGM01) & ~_BV(WGM00);
 
-	TCCR0B = ~_BV(FOC0A) | ~_BV(FOC0B) | ~_BV(WGM02) | ~_BV(CS02) |
-		_BV(CS01) | ~_BV(CS00);
+	TCCR0B = (uint8_t) ~_BV(FOC0A) & ~_BV(FOC0B) & ~_BV(WGM02) & ~_BV(CS02) &
+		~_BV(CS01) & ~_BV(CS00);
 }
 
-uint8_t Timer8b::getDeltaTime(){
-	uint8_t tmp = TCNT0 - old_time;
-	old_time = TCNT0;
-	return tmp;
+
+void Timer8b::start(Prescale prescale, uint8_t initialTime){
+	TCNT0 = initialTime;	//Set timer counter
+	TCCR0B |= prescale;	//Set clk prescale
 }
+
+void Timer8b::pause(){
+	//No clk source, timer stopped.
+	TCCR0B &= ~_BV(CS02) & ~_BV(CS01) & ~_BV(CS00);
+}
+
+void Timer8b::stop(){
+	//No clk source, timer stopped.
+	TCCR0B &= ~_BV(CS02) & ~_BV(CS01) & ~_BV(CS00);
+	TCNT0 = 0;	//Reset timer counter
+}
+
+void Timer8b::reset(){
+	TCNT0 = 0;	//Reset timer counter
+}
+
--- timer8b.h
+++ timer8b.h
@@ -2,18 +2,23 @@
 #define TIMER8B_H
 #include <avr/io.h>
 
+namespace Timer8b_Const{
+	typedef enum {CLK_0 = 1, CLK_8, CLK_64, CLK_256, CLK_1024} Prescale;
+}
+
 class Timer8b{
 	public:
 		//Constructor
 		Timer8b();
-		//Returns the time since last call
-		uint8_t getDeltaTime();
+
+		//Start, stop, pause and reset timer
+		void start(Timer8b_Const::Prescale prescale = Timer8b_Const::CLK_8, uint8_t initialTime = 0);
+		void pause();
+		void stop();
+		void reset();
 
 		//Reference to timer register
 		//uint8_t& time = TCNT0;
-
-	private:
-		uint8_t old_time;
 };
 
 #endif
