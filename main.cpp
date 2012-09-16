683b2b5d4e2b9c73c221c3566b0049f94c88af4c
Carlos Falgueras García <fark.zano@gmail.com>
2012-09-16 03:49:29 +0200

    Editado main para probar algoritmo de Madgwick.

--- main.cpp
+++ main.cpp
@@ -2,103 +2,107 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "Wire.h"
-#include <math.h>
 
-#include "timer8b.h"
+#include "algorithm.h"
+#include "timer16b.h"
 #include "frame.h"
 #include "gyroscope.h"
 #include "accelerometer.h"
 #include "magnetometer.h"
-#include "AHRS.h"
 #include "USART.h"
 #include "vector.h"
+#include "quaternion.h"
 
 extern "C" void __cxa_pure_virtual(void) {
 	while(1);
 }
 
-//Structs for storing sensors data
-SensorData sen_data = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-//Frame to send
-Frame frame;
-
 int main()
 {
+	int count = 50;
+	//Frame to send
+	Frame frame = {{0, 1,0,0,0, 0,0,0, 0,0,0, 0,0,0}};
+
 	//Vectores de los sensores
 	Vector<int> vectAcce(0,0,0);
-	Vector<int> vectMagn(0,0,0);
+	Vector<float> vectMagn(0,0,0);
 	Vector<float> vectGyro(0,0,0);
 	float temperature = 0;
 	
-	//Variables for calculate delta time
-	uint8_t timeStamp = 0;
-	uint8_t ahrsDt_old= 0;
-	uint8_t ahrsDt= 0;
-
-	//Flags for controlling the sampling of the sensors
-	bool aRdy = false;
-	bool gRdy = false;
-	bool mRdy = false;
-
 	//Configure LED pin for debug
 	DDRB |= _BV(PB5);
-	PORTB |= _BV(PB5);
+	PORTB &= ~_BV(PB5);
 
 	sei();	//Enable interrupts
 
 	Wire.begin();    	//Init the I2C
 	_delay_ms(20);
-	USART usart(9600);	//Init the USART
+	Algorithm algorithm;
+	USART usart;		//Init the USART
 	Gyroscope gyr;		//Init the Gyroscope
-	Accelerometer acc;	//Init the Gyroscope
-	Magnetometer mgt;	//Init the Gyroscope
-	Timer8b timer;		//Init the Gyroscope
+	Accelerometer acc;	//Init the Accelerometer
+	Magnetometer mgt;	//Init the Magnetometer
+	_delay_ms(100);
+	Timer16b timer;		//Init the Timmer
 
-	timer.start(Timer8b_Const::CLK_64);	//timer start
+	timer.start(Timer16b_Const::CLK_8);	//timer start
+
+	uint16_t deltaT = 0;
+	float fDeltaT = 0;
+	uint16_t deltaT_old = 0;
+
+	//algorithm.calibration(acc, mgt);
 
 	while(1){
+		if(mgt.getData(vectMagn)){
+			while(!gyr.getData(vectGyro, temperature));
+			algorithm.gyroscope(vectGyro, fDeltaT);
+			algorithm.magnetometer(vectMagn, fDeltaT);
+			algorithm.fusion(fDeltaT);
+			algorithm.oarOrientationCorrection();
+			algorithm.correction();
 
-		gRdy = gyr.getData(vectGyro, temperature);
-		aRdy = acc.getData(vectAcce);
-		mRdy = mgt.getData(vectMagn);
-
-		if(aRdy && gRdy && mRdy){
-			cli();
-			timeStamp = TCNT0;
-			ahrsDt = timeStamp - ahrsDt_old;
-			ahrsDt_old = timeStamp;
-			sei();
-			AHRSupdate(&sen_data, ahrsDt*6.25e-6);	//6.25e-6 -> half of Period
-			aRdy = false;
-			gRdy = false;
-			mRdy = false;
-		}
-
-		if (TCNT0 > 255/4)
-		{
-			frame.roll = atan(2*(q0*q1 + q2*q3)/(1-2*(q1*q1 + q2*q2)));
-			frame.roll = q0 + q1 + q2 + q3;
-			frame.pitch = asin(2*(q0*q2 - q1*q3));
-			frame.yaw = atan(2*(q0*q3 + q1*q2)/(1-2*(q2*q2 + q3*q3)));
-
-			frame.ax = (float) vectAcce.x;
-			frame.ay = (float) vectAcce.y;
-			frame.az = (float) vectAcce.z;
-
-			frame.mx = vectMagn.x;
-			frame.my = vectMagn.y;
-			frame.mz = vectMagn.z;
+			frame.q0 = algorithm.SEq.q0;
+			frame.q1 = algorithm.SEq.q1;
+			frame.q2 = algorithm.SEq.q2;
+			frame.q3 = algorithm.SEq.q3;
 
 			frame.gx = vectGyro.x;
 			frame.gy = vectGyro.y;
 			frame.gz = vectGyro.z;
 
-			frame.time++;
+			frame.ax = algorithm.Ms.x;
+			frame.ay = algorithm.Ms.y;
+			frame.az = algorithm.Ms.z;
 
-			usart.sendFrame(frame.buff, sizeof(Frame));
+			frame.mx = vectMagn.x;
+			frame.my = vectMagn.y;
+			frame.mz = vectMagn.z;
+
+			frame.time += fDeltaT;
+
+			//usart.sendFrame(frame.buff, sizeof(Frame));
+
+			//StatusLEDToggle
+			//PORTB ^= _BV(PB5);
+
+			cli();
+			deltaT = TCNT1;
+			timer.reset();
+			sei();
+			fDeltaT = (float(deltaT))*1e-6;
 		}
 
-		//StatusLEDToggle
-		//PORTB ^= _BV(PB5);
+		if(!count){
+			usart.sendFrame(frame.buff, sizeof(Frame));
+			frame.time = 0;
+			count = 10;
+		}
+		--count;
 	}
 }
+
+//Detección de overflow en el timer
+//ISR(TIMER1_OVF_vect){
+//	PORTB |= _BV(PB5);
+//}
